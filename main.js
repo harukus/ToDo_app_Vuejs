9368f45fd7b74dbab3caae83a5a7223edceada7c
harukus <43bw.hana@gmail.com>
2020-12-27 17:47:41 +0900

    apply ESlint

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 Vue.createApp({
-    data(){
-        return {
-            todos: [],
-            newTodo: null
-        }
-    },
-    mounted() {
+  data() {
+    return {
+      todos: [],
+      newTodo: null,
+    };
+  },
+  mounted() {
     if (localStorage.getItem('todos')) {
       try {
         this.todos = JSON.parse(localStorage.getItem('todos'));
-      } catch(e) {
+      } catch (e) {
         localStorage.removeItem('todos');
       }
     }
-   },
-    methods:{
-        addTodo(){
-            if(!this.newTodo){
-                return;
-            }
-            this.todos.push(this.newTodo);
-            this.newTodo = '';
-            this.saveTodos();
-        },
-        saveTodos(){
-            const parsed = JSON.stringify(this.todos);
-            localStorage.setItem('todos', parsed);
-        },
-        removeTodo(x){
-            this.todos.splice(x, 1);
-            this.saveTodos();
-        }
-    }
+  },
+  methods: {
+    addTodo() {
+      if (!this.newTodo) {
+        return;
+      }
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+      this.saveTodos();
+    },
+    saveTodos() {
+      const parsed = JSON.stringify(this.todos);
+      localStorage.setItem('todos', parsed);
+    },
+    removeTodo(x) {
+      this.todos.splice(x, 1);
+      this.saveTodos();
+    },
+  },
 }).mount('#app');
