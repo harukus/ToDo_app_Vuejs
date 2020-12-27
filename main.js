6f84fe51f344dfbda81ea482c35c335d427dbc3e
harukus <43bw.hana@gmail.com>
2020-12-27 17:20:52 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+    data(){
+        return {
+            todos: [],
+            newTodo: null
+        }
+    },
+    mounted() {
+    if (localStorage.getItem('todos')) {
+      try {
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+      } catch(e) {
+        localStorage.removeItem('todos');
+      }
+    }
+   },
+    methods:{
+        addTodo(){
+            if(!this.newTodo){
+                return;
+            }
+            this.todos.push(this.newTodo);
+            this.newTodo = '';
+            this.saveTodos();
+        },
+        saveTodos(){
+            const parsed = JSON.stringify(this.todos);
+            localStorage.setItem('todos', parsed);
+        },
+        removeTodo(x){
+            this.todos.splice(x, 1);
+            this.saveTodos();
+        }
+    }
+}).mount('#app');
